eb8746cd3be1f7187d25a4f88a4590ca778e592e
Lavinia Medeiros Miranda <laviniameds@icloud.com>
2018-03-11 18:21:15 -0300

    ->servlets de cadastro/impressao atualizados;

--- src/java/servlets/ServletCadastro.java
+++ src/java/servlets/ServletCadastro.java
@@ -5,13 +5,18 @@
  */
 package servlets;
 
+import controller.Agenda;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Contato;
 
 /**
  *
@@ -19,68 +24,39 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "ServletCadastro", urlPatterns = {"/ServletCadastro"})
 public class ServletCadastro extends HttpServlet {
+    
+    Agenda agenda;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    /*protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletCadastro</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletCadastro at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }*/
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         /*processRequest(request, response);*/
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        /*HttpSession sessao = request.getSession();
+        Agenda agenda = (Agenda) sessao.getAttribute("agenda");*/
+        agenda = (Agenda)request.getAttribute("agenda");
+        if(agenda == null)
+            agenda = new Agenda();
         
+        Contato contato = new Contato();
+        contato.setNome("nome");
         
+        try {
+            agenda.inserirContato(contato);
+        } catch (Exception ex) {
+            Logger.getLogger(ServletCadastro.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        request.setAttribute("agenda", agenda);
+        
+        getServletContext().getRequestDispatcher("/ServletImpressao").forward(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
---
+++ src/java/servlets/ServletImpressao.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import controller.Agenda;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Contato;
+
+/**
+ *
+ * @author lavinia
+ */
+@WebServlet(name = "ServletImpressao", urlPatterns = {"/ServletImpressao"})
+public class ServletImpressao extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        Agenda agenda = (Agenda)request.getAttribute("agenda");
+        
+        PrintWriter out = response.getWriter();
+        
+        out.println("<h1>Contatos</h1>\n");
+        out.println("<table style=\"width:70%; border: 1px solid black;\">\n");
+        out.println("<tr><th>Nome</th>");
+        out.println("<th>E-mail</th>");
+        out.println("<th>Telefone</th>");
+        out.println("<th>Endereço</th>");
+        out.println("<th>Detalhes</th></tr>");
+        
+        for(Contato cont:agenda.getAgenda()){
+            out.println("<tr><td>" + cont.getNome()+ "<td></tr>");
+        }
+        
+        out.println("</table><br><br>");
+        
+        out.println("<p>que que<a href='CadastrarContato.html'>Cadastrar</a></p>");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
